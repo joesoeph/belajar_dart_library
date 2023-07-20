f598820e5b1bb8bb446712ccde5be352d1e1a1d4
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-07-21 06:40:57 +0700

    feat:dart library

---
+++ example/hello.dart
@@ -0,0 +1,13 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayHello('Jane Dane'));
+
+  var product = Product();
+  var category = Category();
+  var customer = Customer();
+
+  print(product);
+  print(category);
+  print(customer);
+}
